9c6d7118a9c557c607041eb6521e00a343aec839
Ilya Ryabtsev <binby133@gmail.com>
2023-03-13 12:46:15 +0300

    profsouz update

---
+++ common-files/files/test/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=windows-1251" />
+<title>Главная - ПРОФСОЮЗ НБРБ</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<body>
+
+
+<div id="container">
+
+	<!-- ### Header ### -->
+
+	<div id="header">
+
+	<table border=0 height="150" width="100%">
+	<tr>
+	<td width="100">
+	<img src="images/logo2.gif"></td>
+	<td>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	    <img src="images/ОПО_300_circle2.gif" height="150">
+	</td>
+
+	<td align="right">
+		<img src="images/profsouz.gif">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    	</td>
+
+
+</td>
+	</tr>
+	</table>
+
+
+		<!-- ### Top Menu ### -->
+
+
+	</div>
+
+	<!-- ### Content ### -->
+
+	<div id="contentcontainer">
+
+<div id="con"><center><strong><a href="mrazdel.php?id=3">СПОРТ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="mrazdel.php?id=6">ОЗДОРОВЛЕНИЕ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="mrazdel.php?id=7">КОМИССИЯ ПО ТРУДОВЫМ СПОРАМ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="mrazdel.php?id=13">НОВОСТИ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="mrazdel.php?id=11">ГАЛЕРЕЯ ЛИДЕРОВ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="mrazdel.php?id=10">СТРАХОВАНИЕ</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</center></div>
+		<div id="sidebar">
+
+		<ul>
+
+
+
+		<li><h2>Разделы</h2>
+
+<div id="news"><a href="razdel.php?id=3">Состав                                                                                                                                                                                                                                                         </a></div><div id="news"><a href="razdel.php?id=10">История                                                                                                                                                                                                                                                        </a></div><div id="news"><a href="razdel.php?id=13">Работа профсоюзного комитета Объединенной профсоюзной организации                                                                                                                                                                                              </a></div><div id="news"><a href="razdel.php?id=11">ППО Центрального аппарата НБ РБ                                                                                                                                                                                                                                </a></div><div id="news"><a href="razdel.php?id=26">ППО Центрального хранилища НБ РБ                                                                                                                                                                                                                               </a></div><div id="news"><a href="razdel.php?id=27">ППО Учебного центра НБ РБ                                                                                                                                                                                                                                      </a></div><div id="news"><a href="razdel.php?id=6">О профсоюзе                                                                                                                                                                                                                                                    </a></div><div id="news"><a href="razdel.php?id=7">Документы                                                                                                                                                                                                                                                      </a></div><div id="news"><a href="razdel.php?id=2">Положение                                                                                                                                                                                                                                                      </a></div><div id="news"><a href="razdel.php?id=24">Устав                                                                                                                                                                                                                                                          </a></div><div id="news"><a href="razdel.php?id=23">Генеральное соглашение между Правительством Республики Беларусь, республиканскими объединениями нанимателей и профсоюзов на 2019 – 2021 годы                                                                                                                   </a></div><div id="news"><a href="razdel.php?id=15">Коллективный договор                                                                                                                                                                                                                                           </a></div><div id="news"><a href="razdel.php?id=14">Формы заявлений в ОПО РБ                                                                                                                                                                                                                                       </a></div><div id="news"><a href="razdel.php?id=8">Сотрудничество                                                                                                                                                                                                                                                 </a></div><div id="news"><a href="razdel.php?id=9">Партнеры                                                                                                                                                                                                                                                       </a></div>
+
+
+		</li>
+
+
+
+		<li><h2><a href="http://master:1111/">Корпоративный портал</a></h2></li>
+
+                    
+
+           		<li><h2><a href="mailto:N.kovchur@nbrb.by                                 ">Обратная связь</a></h2></li>
+           		</ul>
+           <center>Председатель ОПО<br/>
+<b>Ковчур Наталья Петровна</b><br/>
+тел. 017 219 23 57 (вн. тел. 13 57)<br/><br/>
+
+Заместитель председателя ОПО<br/>
+<b>Слободчикова Любовь Валерьевна</b><br/>
+тел. 017 219 23 27 (вн.тел. 1327)<br/>
+<br/>
+Главный бухгалтер<br/>
+<b>Масуми Марина Николаевна</b><br/>
+тел. 017 215 35 42 (вн.тел. 1242)<br/>
+моб.тел. 8 029 187 78 74<br/><br/>
+<br/>
+Заместитель председателя<br/>ППО ЦА и УЦ ОПО
+<br/>
+<b>Сивец Вячеслав Леонидович</b><br/>
+тел. 017 219 23 08 (вн.тел. 1308)
+<!--
+<br/><br/>
+Казначей<br/>
+
+<b>Вислоус Галина Васильевна</b><br/>
+тел. 017 215 35 42 (вн.тел. 1542)
+--></center>
+
+
+
+		</div>
+
+
+      <div id="content">
+
+		<!-- ### Post Entry Begin ###  -->
+
+
+		<div class="post"><br/>
+      Главная
+    	<p class="posted"></p>  <br/>
+		<p style="text-align: right;"><em><span style="font-size: medium; color: #800000;"><strong>ЦЕЛЬЮ ЕДИНЫ, ЕДИНСТВОМ СИЛЬНЫ!</strong></span></em></p>
+<p style="text-align: left;"><span style="color: #800000; font-size: small;"><strong>Уважаемые работники, члены профсоюза и посетители корпоративного портала Национального банка!</strong></span></p>
+<p style="text-align: center;"><span style="color: #800000;">Рады представить вам страницу Объединенной профсоюзной организации Национального банка Республики Беларусь Белорусского профсоюза работников государственных и других учреждений (далее &ndash; профсоюзная организация Национального банка, профсоюз), размещенную&nbsp;на корпоративном информационном ресурсе Национального банка.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">На данной странице в структурированном виде находится вся информация, касающаяся деятельности профсоюзной организации Национального банка, организации ее работы и проводимых ею мероприятий и акций. Страница профсоюзной организации Национального банка выполнена в корпоративном стиле и содержит два навигационных меню.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Коротко остановимся на основных разделах и рубриках страницы профсоюзной организации Национального банка.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Верхнее горизонтальное навигационное меню содержит разделы: афиша, акции, спорт, экскурсии и рубрику &laquo;Поздравляем&raquo;.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">В разделе АФИША вы можете найти репертуары театров на месяц, объявления о ближайших музыкально-развлекательных программах, концертах, анонсы.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">В АКЦИЯХ публикуются анонсы и объявления о различных организуемых конкурсах, выставках, смотрах, благотворительных акциях.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">СПОРТ предназначен для размещения информации о планируемых физкультурно-оздоровительных мероприятиях как в пределах Национального банка, так и за его пределами, об абонементах, отчетов, включая фотоотчеты, о спортивных состязаниях&nbsp;и поздравлений призеров.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Объявления об экскурсиях, проводимых с привлечением средств профсоюзной организации Национального банка, размещаются в разделе ЭКСКУРСИИ.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">В рубрике &laquo;Поздравляем&raquo; публикуются слова признательности, пожелания как по случаю праздничных дней, так и по случаю поздравления отдельных членов профсоюзной организации Национального банка.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Боковое вертикальное навигационное меню состоит из: устав, положение, состав, деятельность, план работы на год, о профсоюзе, документы, сотрудничество, партнеры, история, Центральный аппарат и Учебный центр.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">С разделами УСТАВ, ПОЛОЖЕНИЕ, ДОКУМЕНТЫ и ПЛАН РАБОТЫ НА ГОД все предельно ясно.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">В разделе СОСТАВ приведен перечень лиц, входящих в профсоюзный комитет Объединенной профсоюзной организации Национального банка Республики Беларусь Белорусского профсоюза работников государственных и других учреждений.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Объявления о планируемых профсоюзных конференциях, а также информацию по итогам их проведения, включая отдельные протоколы, можно найти в разделе ДЕЯТЕЛЬНОСТЬ.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Пошаговая инструкция по вступлению с бланками заявлений, а также основные преимущества вступления в профсоюз находятся в разделе О ПРОФСОЮЗЕ.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Разделы СОТРУДНИЧЕСТВО и ПАРТНЕРЫ содержат перечень организаций, которые объединяет Белорусский профсоюз работников государственных и других учреждений, и перечень общественных объединений и организаций, с которым сотрудничает профсоюз в рамках Национального банка, соответственно.</span></p>
+<p style="text-align: justify;"><span style="color: #800000;">Краткую историческую справку&nbsp;о профсоюзном движении в стране можно найти в разделе ИСТОРИЯ.</span></p>
+<p><span style="font-family: %value; font-size: small;"><strong><em><em><span style="font-size: medium; color: #800000;"><a href="files/file/temp/istoriya.doc">Из истории 1 мая</a>&nbsp;</span></em></em></strong></span></p></div>
+
+		</div>
+
+<div id="foot"></div>
+<div id="footer">
+<p>
+	Copyright &copy; 2013 <a href="http://www.nbrb.by" title="Официальный сайт Национального банка Республики Беларусь в глобальной компьютерной сети Интернет">Национальный банк Республики Беларусь</a>
+</p>
+</div>
+
+	</div>
+</div>
+
